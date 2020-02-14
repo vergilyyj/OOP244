e1627c441f6c7e2bfba4ed4958fab7936ffae759
vergilyyj <43761834+vergilyyj@users.noreply.github.com>
2020-02-13 19:28:05 -0500

    Add files via upload

---
+++ Workshop1/Tools.h
@@ -0,0 +1,22 @@
+/***********************************************************************
+// 244-200 Workshop 1: Compiling modular source code
+// File	Tools.h
+// Version 1.0
+// Date	2020/01/17
+// Author	Ya Jian Yu
+// Description
+// This provide the declearation for the tools
+//
+// Revision History
+// -----------------------------------------------------------
+// Name            Date            Reason
+// Ya Jian Yu
+/////////////////////////////////////////////////////////////////
+***********************************************************************/
+#ifndef SDDS_TOOLS_H__ 
+#define SDDS_TOOLS_H__
+
+namespace sdds {
+   void toLowerCaseAndAlphabeticCopy(char des[], const char source[]);
+}
+#endif
